cd77a0d5e23d155e6a627543085ddfa14f39335d
Huyen059 <samihuyen059@gmail.com>
2020-10-11 13:54:17 +0200

    use servlet and JSP to display response of a get request

---
+++ src/main/java/com/airline/controllers/AddPassenger.java
@@ -0,0 +1,30 @@
+package com.airline.controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "AddPassenger", urlPatterns = {"/AddPassenger"})
+public class AddPassenger extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Write directly to the view
+//        PrintWriter out = response.getWriter();
+//        response.setContentType("text/html");
+//        out.println("<html><body>");
+//        out.println("<h3>Hello From Servlet</h3>");
+//        out.println("</html></body>");
+
+        // Forward to JSP
+        RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/add-passenger.jsp");
+        view.forward(request, response);
+    }
+}
---
+++ target/airlineJee-1.0-SNAPSHOT/WEB-INF/views/add-passenger.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: becode
+  Date: 10/11/20
+  Time: 1:41 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Airline</title>
+</head>
+<body>
+    <h3>Hello from add-passenger JSP</h3>
+</body>
+</html>
